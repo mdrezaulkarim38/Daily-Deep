48d7e87636c7b4e0c020efcea97880ef4215f413
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2024-07-08 22:03:10 +0600

    Add new page and controller

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Daily_Deep.Controllers
+{
+    [Route("[controller]")]
+    public class AuthController : Controller
+    {
+        private readonly ILogger<AuthController> _logger;
+
+        public AuthController(ILogger<AuthController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("/Login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
