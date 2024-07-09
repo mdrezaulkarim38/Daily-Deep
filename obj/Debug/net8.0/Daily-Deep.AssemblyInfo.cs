5379859fcd518c1c8dc7bc598027a22402db80ce
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2024-07-09 09:37:24 +0600

    full login, Register and logout system done

--- obj/Debug/net8.0/Daily-Deep.AssemblyInfo.cs
+++ obj/Debug/net8.0/Daily-Deep.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Daily-Deep")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bcb0fc74a9ec867ac24aa356760e33fc90c0251")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31f8c258da4ff4925d41c5f0a34a30c3ea04362f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Daily-Deep")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Daily-Deep")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
