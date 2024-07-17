5c1ff9dbb1e06e617436309c3ecdc84473da8d8c
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2024-07-17 21:59:17 +0600

    Create model users

---
+++ Models/Users.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+namespace Daily_Deep.Models;
+
+public class Users
+{
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    [Display(Name = "Full Name")]
+    [StringLength(100, ErrorMessage = "Full Name Cannot be Longer than 100 characters.")]
+    public string? FullName { get; set; }  
+
+    [Required]
+    [Display(Name = "Username")]
+    [StringLength(50, ErrorMessage = "Username cannot be longer than 50 characters.")]
+    public string? Username { get; set; }
+
+    [EmailAddress]
+    [Display(Name = "Email")]
+    public string? Email { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    [Display(Name = "Password")]
+    [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters long.")]
+    public string? Password { get; set; }
+}
