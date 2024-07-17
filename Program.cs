a4e820446a23fad4ab11fb98f6a341c19b7bc87c
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2024-07-17 22:28:59 +0600

    add Interfaces

--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options => {
+    options.Cookie.Name = "UserLoginCookie";
+    options.LoginPath = "/Auth/Login";
+});
+
 
 var app = builder.Build();
 
