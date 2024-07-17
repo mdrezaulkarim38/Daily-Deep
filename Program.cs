2a43092ab240479cb17b0d1f192b9b455fd5caa7
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2024-07-17 21:24:48 +0600

    Remove all code

--- Program.cs
+++ Program.cs
@@ -1,22 +1,7 @@
-using Daily_Deep.Interfaces;
-using Daily_Deep.Services;
-using Microsoft.AspNetCore.Authentication.Cookies;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
-// Add cookie authentication
-builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-    .AddCookie(options =>
-    {
-        options.Cookie.Name = "UserLoginCookie";
-        options.LoginPath = "/Auth/Login";
-    });
-
-// Register the AccountService with the DI container
-builder.Services.AddScoped<IAccountService, AccountService>();
 
 var app = builder.Build();
 
@@ -24,6 +9,7 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -32,8 +18,6 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-// Add authentication and authorization middleware
-app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
