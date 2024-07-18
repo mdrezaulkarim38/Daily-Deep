7cb45ed089258bb9e53ad363145f5102419300b7
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2024-07-18 21:39:45 +0600

    Category new Model add done

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,3 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Daily_Deep.Models;
 public class Category
-{}
+{
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    [Display(Name = "Category Name")]
+    [StringLength(50, ErrorMessage = "Category Name Cannot be Longer then 50 characters.")]
+    public string? CategoryName { get; set; }
+    
+    [Required]
+    [Display(Name = "Category Code")]
+    public int? CategoryCode { get; set; }
+
+    [Required]
+    [Display(Name = "User Id")]
+    public int? UserId { get; set; }
+}
